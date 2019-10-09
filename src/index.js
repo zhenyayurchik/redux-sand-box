1e75f95e182cb4ddf36ec1b246b65d2358191b42
zhenyayurchik <joomi94@yandex.ru>
2019-10-09 23:57:11 +0300

    Add app

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { createStore } from 'redux';
+
+const reduser = (state = 0, action) => {
+
+    switch (action.type){
+        case'INC':
+            return state + 1;
+        case'DEC':
+            return state - 1;
+        case'RND':
+            return state + action.payload;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reduser);
+
+document.getElementById('inc')
+        .addEventListener('click', ()=>{
+            store.dispatch({type: 'INC'});
+        });
+
+document.getElementById('dec')
+        .addEventListener('click', ()=>{
+            store.dispatch({type: 'DEC'})
+        })
+
+document.getElementById('rnd')
+        .addEventListener('click', ()=>{
+            const payload = Math.floor(Math.random()*10);
+            store.dispatch({
+                type: 'RND',
+                payload
+            });
+        });
+const update = ()=>{
+    document.getElementById('counter')
+            .innerHTML = store.getState();
+};
+
+store.subscribe(update);
